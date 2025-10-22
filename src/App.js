992774e9a66cb37222b0c7597e80484bed503259
ciscoxz <pedroceschim123@gmail.com>
2025-10-22 17:32:04 -0300

    produtos e contato

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './css/App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contato from './Telas/Contato/Contato'
+import Produtos from './Telas/Produtos/Produtos'
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-     <h1>Site em React</h1>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/Contato">Contato</Link>
+        <Link to="/Produtos">Produtos</Link>
+      </nav>
+      <div style={{ textAlign: 'center' }}>
+      <h1>Site em React</h1>
+      </div>
+      <Routes>
+        <Route path="/Contato" element={<Contato />} />
+        <Route path="/Produtos" element={<Produtos />} />
+      </Routes>
+    </Router>
   );
 }
 
